c6edbbfd794c0ab8f7f6b873bed7a62cf50e0595
Seung taek Oh <elqkddl@naver.com>
2023-05-10 21:27:02 +0900

    [Init] 환경설정

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
