cf62a67dce56089acc055a2e612a1a9785dd5c15
Seung taek Oh <elqkddl@naver.com>
2023-05-11 14:35:10 +0900

    [learn] 이미지 넣는 법 & Public 폴더 이용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Navbar.Brand href="#home">Wintaek</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -15,6 +15,29 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col sm>
+            <img src="https://openimage.interpark.com/goods_image_big/8/9/6/9/11014608969_l.jpg" width="80%"></img>
+            <h4>Adidas</h4>
+            <p>Adidas Samba ADV Cardboard</p>
+            <p style={{ fontSize: '14px', color: 'gray' }}>아디다스 삼바 ADV 카드보드</p>
+          </Col>
+          <Col sm>
+            <img src="https://openimage.interpark.com/goods_image_big/8/9/6/9/11014608969_l.jpg" width="80%"></img>
+            <h4>Adidas</h4>
+            <p>Adidas Samba ADV Cardboard</p>
+            <p style={{ fontSize: '14px', color: 'gray' }}>아디다스 삼바 ADV 카드보드</p>
+          </Col>
+          <Col sm>
+            <img src="https://openimage.interpark.com/goods_image_big/8/9/6/9/11014608969_l.jpg" width="80%"></img>
+            <h4>Adidas</h4>
+            <p>Adidas Samba ADV Cardboard</p>
+            <p style={{ fontSize: '14px', color: 'gray' }}>아디다스 삼바 ADV 카드보드</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
