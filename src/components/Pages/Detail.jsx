0ad0cd3b1108657cd098f33cb0a47d78fb6252f3
Seung taek Oh <elqkddl@naver.com>
2023-05-19 16:50:37 +0900

    [Assignment] <input>에 숫자말고 다른거 입력하면 경고창 띄우기

--- src/components/Pages/Detail.jsx
+++ src/components/Pages/Detail.jsx
@@ -6,23 +6,39 @@ export function Detail(props) {
   const { id } = useParams();
   const photo = props.shoes.find((item) => item.id === parseInt(id));
   const [showAlert, setAlert] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+  const [Warning, setWarning] = useState(false);
   // console.log(photo);
 
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    // console.log('1');
+    if (/^\d*$/.test(value)) {
+      // 숫자만 입력가능한 정규표현식
+      setInputValue(value);
+      setWarning(false);
+    } else {
+      setInputValue('');
+      setWarning(true);
+    }
+  };
   useEffect(() => {
     const timer = setTimeout(() => {
       setAlert(false);
-      console.log(1);
     }, 2000);
 
     return () => {
-      console.log(2);
       clearTimeout(timer);
     };
   }, []);
 
   return (
     <div className="container">
-      {showAlert && <div className="alert alert-warning">2초이내 구매시 할인</div>}
+      {showAlert && (
+        <div className="alert alert-warning">
+          2초이내 구매시 할인 <button>구매</button>
+        </div>
+      )}
       <div className="row">
         <div className="col-md-6">
           <img src={photo.imageUrl} alt={photo.title} width="80%" />
@@ -33,6 +49,8 @@ export function Detail(props) {
           <Content>{props.shoes[id].content}</Content>
           <p>{props.shoes[id].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</p>
           <button className="btn btn-danger">주문하기</button>
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          {Warning && alert('숫자만 입력하세요')}
         </div>
       </div>
     </div>
