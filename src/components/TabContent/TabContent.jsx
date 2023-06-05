0d9b5efb701fee6a3ec9c058505ef2f09cf5ae1d
Seung taek Oh <elqkddl@naver.com>
2023-06-05 17:11:30 +0900

    [learn] 컴포넌트 애니메이션 주는 법

--- src/components/TabContent/TabContent.jsx
+++ src/components/TabContent/TabContent.jsx
@@ -1,11 +1,20 @@
-export function TabContent(props) {
-  if (props.tab === 0) {
-    return <div>내용1</div>;
-  }
-  if (props.tab === 1) {
-    return <div>내용2</div>;
-  }
-  if (props.tab === 2) {
-    return <div>내용3</div>;
-  }
+import { useEffect, useState } from 'react';
+
+export function TabContent({ tab }) {
+  const [fade, setFade] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFade('end');
+    }, 10);
+
+    return () => {
+      setFade('');
+    };
+  }, [tab]);
+  return (
+    <div className={'start ' + fade}>
+      {[<div>내용1</div>, <div>내용2</div>, <div>내용3</div>][tab]}
+    </div>
+  );
 }
